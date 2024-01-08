521bf2f9d1236d52a962718ba7f640add95fbe84
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-01-08 20:02:36 -0300

    ...

---
+++ tasklite/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import Menu from '../components/Menu.jsx'
+
+export default function Home() {
+    return (
+        <>
+            <Menu />
+            <h1>Pagina inicial</h1>
+        </>
+    )
+
+}
