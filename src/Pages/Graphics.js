2802319a9501400bad5da14ae7bc6efd7b64c1c1
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-01-09 13:19:56 -0300

    ...

--- src/Pages/Graphics.js
+++ src/Pages/Graphics.js
@@ -2,9 +2,9 @@ import '../css/App.css'
 
 export default function Graphics() {
     return (
-        <>
+        <main id='content'>
             <h1>Graphics</h1>
-        </>
+        </main>
     )
 
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,22 @@
 import '../css/App.css'
+import '../css/Home.css'
 
-export default function Home() {
+import React from 'react'
+
+const Home = () => {
     return (
-        <>
-            <h1>Página Inicial</h1>
-        </>
+        <main id='content'>
+            <div className='presentation'>
+                    <button className='day'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 30 30" fill="none">
+                            <path d="M0.042796 0V7.5H30V0H0.042796ZM0.042796 11.25V29.6625C0.042796 29.85 0.21398 30 0.42796 30H29.572C29.786 30 29.9572 29.85 29.9572 29.6625V11.25H0H0.042796ZM4.3224 15H8.602V18.75H4.3224V15ZM12.8816 15H17.1612V18.75H12.8816V15ZM21.4408 15H25.7204V18.75H21.4408V15ZM4.3224 22.5H8.602V26.25H4.3224V22.5ZM12.8816 22.5H17.1612V26.25H12.8816V22.5Z" fill="#F9F9F9"/>
+                        </svg>
+                        <p>Jan 09.2024</p>
+                        <p>13:00 pm</p>
+                    </button>
+            </div>
+        </main>
     )
 }
+
+export default Home;
