65f7270abd14206e15dbdbd726af2f10a1cb9243
lucasramallo <lucasdebrito.jd@gmail.com>
2024-01-10 23:29:22 -0300

    refactor: Criando router.js e tirando responsabilidades do index.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import Home from './Pages/Home'
