5a2447df503701989030b459402f85500f432fb0
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-01-21 11:41:29 -0300

    ...

--- src/Pages/Graphics.js
+++ src/Pages/Graphics.js
@@ -3,7 +3,11 @@ import '../css/App.css'
 export default function Graphics() {
     return (
         <main id='content'>
-            <h1>Graphics</h1>
+            <h1>Completede Tasks</h1>
+            <div className='graph-container'>
+                <div className='chart-content'></div>
+                <div className='details'></div>
+            </div>
         </main>
     )
 
